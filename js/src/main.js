ff258ef56a52f9111b2623b68ea7bca97686a9e8
Nick Villapiano <nvillapiano@gmail.com>
2017-08-23 18:59:46 -0400

    Added Typography // Fonts // First pass at Home // Projects

--- js/src/main.js
+++ js/src/main.js
@@ -3,9 +3,50 @@
 // Fade in on page load
 function fadeIn() {
     $(document).ready( function() {
-        $('html, body').addClass('in');
+        $('body').addClass('in');
+        $('body').addClass('no-overflow');
+        $('body').animate({ 
+            scrollTop: "0" 
+        }, 10);        
+        console.log('loaded');
     })
+}
+// Expand homepage sections
+function expandHome() {
+    $('.projects-trigger').click( function() {
+        $('.projects-trigger').toggleClass('expand');
+        //
+        setTimeout( function() {
+            $('.case-studies').toggleClass('show');
+            $('body').toggleClass('no-overflow');
+            $('.circle-wrapper').toggleClass('cream');
+            window.scrollTo(0, 0);
+        }, 300)
+    })  
+    $('.me-trigger').click( function() {
+        $('.me-trigger').toggleClass('expand');
+        //
+        setTimeout( function() {
+            $('.case-studies').toggleClass('show');
+            $('body').toggleClass('no-overflow');
+            $('.circle-wrapper').toggleClass('cream');
+            window.scrollTo(0, 0);
+        }, 300)
+    })  
+    $('.back').click( function() {
+        $('.case-studies').removeClass('show');        
+        //
+        setTimeout( function() {
+            $('.projects-trigger, .me-trigger').removeClass('expand');
+            $('body').toggleClass('no-overflow');  
+            $('.circle-wrapper').toggleClass('cream');          
+        }, 300)
+        $('body').animate({ 
+            scrollTop: "0" 
+        }, 500);
+    })          
 }
 
 /* Call functions */
 fadeIn();
+expandHome();
